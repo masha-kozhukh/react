2183d31ce462abf4bb4c0153d4a9e2d94278b9be
masha <kozhukh@web.paradigm.com.ua>
2025-02-06 14:27:46 +0200

    added styleі for disable button and custom hook for toDoList

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -17,6 +17,65 @@ export type ListsProps = {
   buttonTextEdit: string;
 };
 
+type UseToDoListParams = {
+  initialItems: TextWithId[];
+};
+
+const hooks = {
+  useToDoList({ initialItems }: UseToDoListParams) {
+    const [items, setItems] = React.useState<TextWithId[]>(initialItems);
+    const [newText, setNewText] = React.useState<string>("");
+
+    const handleAddItem = React.useCallback(() => {
+      setItems((previousState) => [
+        ...previousState,
+        { id: Date.now(), text: newText },
+      ]);
+
+      setNewText("");
+    }, [newText, setItems, setNewText]);
+
+    const handleUpdateItem = React.useCallback(
+      (newTextWithId: TextWithId | null) => {
+        if (!newTextWithId || !newTextWithId.text || !newTextWithId.id) {
+          return;
+        }
+
+        setItems((previousState) => {
+          const indexOfUpdatingItem = previousState.findIndex(
+            (item) => item.id === newTextWithId.id
+          );
+
+          if (indexOfUpdatingItem !== -1) {
+            previousState[indexOfUpdatingItem].text = newTextWithId.text;
+          }
+
+          return previousState;
+        });
+      },
+      [setItems]
+    );
+
+    const handleDeleteItem = React.useCallback(
+      (id: number) => {
+        setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+      },
+      [setItems]
+    );
+
+    const handleInputChange = inputHooks.useInputChange(setNewText);
+
+    return {
+      items,
+      newText,
+      handleAddItem,
+      handleUpdateItem,
+      handleDeleteItem,
+      handleInputChange,
+    };
+  },
+};
+
 export const ToDoList = React.memo((props: ListsProps) => {
   const {
     subtitle,
@@ -30,71 +89,44 @@ export const ToDoList = React.memo((props: ListsProps) => {
     buttonTextEdit,
   } = props;
 
-  const [items, setItems] = React.useState<TextWithId[]>(initialItems);
-  const [newText, setNewText] = React.useState<string>("");
-
-  const addItem = React.useCallback(() => {
-    setItems((previousState) => [
-      ...previousState,
-      { id: Date.now(), text: newText },
-    ]);
-
-    setNewText("");
-  }, [newText, setItems, setNewText]);
-
-  const updateItem = React.useCallback(
-    (newTextWithId: TextWithId | null) => {
-      if (!newTextWithId || !newTextWithId.text || !newTextWithId.id) {
-        return;
-      }
-
-      setItems((previousState) => {
-        const indexOfUpdatingItem = previousState.findIndex(
-          (item) => item.id === newTextWithId.id
-        );
-
-        if (indexOfUpdatingItem !== -1) {
-          previousState[indexOfUpdatingItem].text = newTextWithId.text;
-        }
-
-        return previousState;
-      });
-    },
-    [setItems]
-  );
-
-  const deleteItem = React.useCallback(
-    (id: number) => {
-      setItems((prevItems) => prevItems.filter((item) => item.id !== id));
-    },
-    [setItems]
-  );
-
-  const onInputChange = inputHooks.useInputChange(setNewText);
+  const {
+    items,
+    newText,
+    handleAddItem,
+    handleUpdateItem,
+    handleDeleteItem,
+    handleInputChange,
+  } = hooks.useToDoList({
+    initialItems,
+  });
 
   return (
-    <section className="">
+    <section>
       <p className="subTitle">{subtitle}</p>
+
       <h1 className="title">{title}</h1>
+
       <div>
         <Input
-          onChange={onInputChange}
+          onChange={handleInputChange}
           value={newText}
           placeholder={placeholder}
         />
+
         <Button
           disabled={!newText.trim()}
-          onClick={addItem}
+          onClick={handleAddItem}
           name={buttonTextAdd}
           className={classNameAddButton}
         />
+
         <ul>
           {items.map((item) => (
             <ToDoItem
               key={item.id}
               item={item}
-              updateItem={updateItem}
-              deleteItem={deleteItem}
+              updateItem={handleUpdateItem}
+              deleteItem={handleDeleteItem}
               buttonTextRemove={buttonTextRemove}
               buttonTextSave={buttonTextSave}
               buttonTextEdit={buttonTextEdit}
---
+++ src/constants/accordions.ts
@@ -0,0 +1,23 @@
+import { AccordionsProps } from "../components/Accordion";
+
+export const accordions: AccordionsProps = {
+  contents: [
+    {
+      name: "Design",
+      text: "a plan or drawing produced to show the look and function",
+      id: 0,
+    },
+    {
+      name: "Design",
+      text: "a plan or drawing produced to show the look and function",
+      id: 1,
+    },
+    {
+      name: "Design",
+      text: "a plan or drawing produced to show the look and function",
+      id: 2,
+    },
+  ],
+  subtitle: "Our accordions",
+  title: "Why People Choose Accordions",
+};
